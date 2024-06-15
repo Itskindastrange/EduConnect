bee95939b2426d90368203b99372cf532fce6821
abdullah <abdullahahmaddd789@gmail.com>
2024-06-15 14:49:12 +0500

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ClassFiles, ClassRoom, MemberShip, RoomStream, Comment
-
-admin.site.register(ClassRoom)
-admin.site.register(ClassFiles)
-admin.site.register(MemberShip)
-admin.site.register(RoomStream)
-admin.site.register(Comment)
